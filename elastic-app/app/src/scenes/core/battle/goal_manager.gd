44fc3271b86f9e5394fa8ccd04654992791d3a8e
Ross Wolf <rosswolf@gmail.com>
2025-08-06 13:27:47 -0700

    rotating goals

--- elastic-app/app/src/scenes/core/battle/goal_manager.gd
+++ elastic-app/app/src/scenes/core/battle/goal_manager.gd
@@ -2,11 +2,19 @@ extends Node
 
 class_name GoalManager
 
+var active_goal: Goal
+const available_goals = ["survival_1","play_cards_1","activate_buildings_1"]
+
 func _init():
-	#GlobalSignals.core_goal_succeeded.connect()
-	#GlobalSignals.core_goal_failed.connect()
+	GlobalSignals.core_goal_succeeded.connect(__on_goal_complete)
+	GlobalSignals.core_goal_failed.connect(__on_goal_complete)
 	GlobalSignals.core_begin_turn.connect(__on_begin_turn)
 	
 func __on_begin_turn():
-	var goal: Goal = Goal.load_goal("survival_1")
-	GlobalSignals.signal_core_goal_created(goal.instance_id)
+	active_goal = Goal.load_goal(available_goals.pick_random())
+	GlobalSignals.signal_core_goal_created(active_goal.instance_id)
+	
+func __on_goal_complete(goal_instance_id: String):
+	if goal_instance_id == active_goal.instance_id:
+		active_goal = Goal.load_goal(available_goals.pick_random())	
+		GlobalSignals.signal_core_goal_created(active_goal.instance_id)
